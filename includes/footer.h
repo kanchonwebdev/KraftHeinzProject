acd51682cd159568513bb573aefb54e1abca17ed
kanchonwebdev <88568490+kanchonwebdev@users.noreply.github.com>
2022-05-30 21:42:24 +0600

    New file uploaded

---
+++ includes/footer.h
@@ -0,0 +1,39 @@
+<div class="footer">
+    <div class="footer-block">
+        <div class="logo">Kraft <span>Heinz</span></div>
+    </div>
+
+    <div class="footer-block">
+        <div class="bold-text">Resources</div>
+
+        <div class="nav-link-block">
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Terms Of Use</a>
+            <a href="#">Privacy Notice</a>
+        </div>
+    </div>
+
+    <div class="footer-block">
+        <div class="bold-text">Related Sites</div>
+
+        <div class="nav-link-block">
+            <a href="#">Kraft Retail</a>
+            <a href="#">Kraft Corporate</a>
+            <a href="#">US Kraft Ingredients</a>
+            <a href="#">US Foodservice</a>
+            <a href="#">US Kraft Ingredients</a>
+            <a href="#">Tassimo Pro</a>
+        </div>
+    </div>
+
+    <div class="footer-block">
+        <div class="bold-text">Newsletter Sign-Up</div>
+
+        <div class="block">
+            <p>Sign up to receive updates, recipes and product news</p>
+
+            <a href="#" class="btn">Sign Up Now</a>
+        </div>
+    </div>
+</div>
---
+++ includes/js/js-bundle.h
@@ -0,0 +1,4 @@
+<script src="assets/js/nav_menu_script.js"></script>
+<script src="assets/js/custom_loader.js"></script>
+<script src="assets/js/scroll_top.js"></script>
+<script src="assets/js/wow.js"></script>
