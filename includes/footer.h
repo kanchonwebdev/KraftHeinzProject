5d0be78514708d748d9b9e1d3fbf68a89ff1d57b
kanchonwebdev <88568490+kanchonwebdev@users.noreply.github.com>
2022-06-03 01:04:35 +0600

    animation added

--- includes/footer.h
+++ includes/footer.h
@@ -1,12 +1,12 @@
 <div class="footer">
     <div class="footer-block">
-        <div class="logo">Kraft <span>Heinz</span></div>
+        <div class="logo wow fadeInUp">Kraft <span>Heinz</span></div>
     </div>
 
     <div class="footer-block">
-        <div class="bold-text">Resources</div>
+        <div class="bold-text wow fadeInUp">Resources</div>
 
-        <div class="nav-link-block">
+        <div class="nav-link-block wow fadeInUp">
             <a href="#">About Us</a>
             <a href="#">Contact Us</a>
             <a href="#">Terms Of Use</a>
@@ -15,9 +15,9 @@
     </div>
 
     <div class="footer-block">
-        <div class="bold-text">Related Sites</div>
+        <div class="bold-text wow fadeInUp">Related Sites</div>
 
-        <div class="nav-link-block">
+        <div class="nav-link-block wow fadeInUp">
             <a href="#">Kraft Retail</a>
             <a href="#">Kraft Corporate</a>
             <a href="#">US Kraft Ingredients</a>
@@ -28,9 +28,9 @@
     </div>
 
     <div class="footer-block">
-        <div class="bold-text">Newsletter Sign-Up</div>
+        <div class="bold-text wow fadeInUp">Newsletter Sign-Up</div>
 
-        <div class="block">
+        <div class="block wow fadeInUp">
             <p>Sign up to receive updates, recipes and product news</p>
 
             <a href="#" class="btn">Sign Up Now</a>
--- includes/js/js-bundle.h
+++ includes/js/js-bundle.h
@@ -2,3 +2,6 @@
 <script src="assets/js/custom_loader.js"></script>
 <script src="assets/js/scroll_top.js"></script>
 <script src="assets/js/wow.js"></script>
+<script>
+    new WOW().init();
+</script>
